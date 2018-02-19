d1f3a4199708a82620f39c8a5410b8df87fafe37
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2018-02-19 13:26:54 +0100

    Poco Poco

--- ClavyMIGSImportPlugin/src/fdi/ucm/server/migs/LoadCollectionMIGS.java
+++ ClavyMIGSImportPlugin/src/fdi/ucm/server/migs/LoadCollectionMIGS.java
@@ -5,11 +5,19 @@ package fdi.ucm.server.migs;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+
+
+
 import fdi.ucm.server.modelComplete.ImportExportDataEnum;
 import fdi.ucm.server.modelComplete.ImportExportPair;
 import fdi.ucm.server.modelComplete.LoadCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteCollection;
 import fdi.ucm.server.modelComplete.collection.CompleteCollectionAndLog;
+import fdi.ucm.server.modelComplete.collection.document.CompleteDocuments;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteGrammar;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteOperationalValueType;
+import fdi.ucm.server.modelComplete.collection.grammar.CompleteTextElementType;
 
 /**
  * @author Joaquin Gayoso Cabada
@@ -23,6 +31,7 @@ public class LoadCollectionMIGS extends LoadCollection{
 //	private CompleteLinkElementType topicIDTC;
 	public static boolean consoleDebug=false;
 	private ArrayList<ImportExportPair> Parametros;
+	private HashMap<Integer, CompleteDocuments> ObjetoVirtual;
 	
 	
 	
@@ -71,7 +80,7 @@ public class LoadCollectionMIGS extends LoadCollection{
 			Logs=new ArrayList<String>();
 			Salida.setLogLines(Logs);
 
-		
+			process_Objetos_Digitales();
 			
 			
 			return Salida;
@@ -83,6 +92,96 @@ public class LoadCollectionMIGS extends LoadCollection{
 	}
 
 	
+
+
+	private void process_Objetos_Digitales() {
+		ObjetoVirtual=new HashMap<Integer, CompleteDocuments>();
+		
+		CompleteGrammar GrammarVO = new CompleteGrammar(NameConstantsMIGS.VIRTUAL_OBJECTNAME, NameConstantsMIGS.VIRTUAL_OBJECTNAME,CC);
+		
+		{
+		String VistaOV=new String(NameConstantsMIGS.PRESNTACION);
+		
+		CompleteOperationalValueType Valor = new CompleteOperationalValueType(NameConstantsMIGS.VISIBLESHOWN,Boolean.toString(true),VistaOV);
+		CompleteOperationalValueType Valor2=new CompleteOperationalValueType(NameConstantsMIGS.BROWSERSHOWN,Boolean.toString(false),VistaOV);
+		CompleteOperationalValueType Valor3=new CompleteOperationalValueType(NameConstantsMIGS.SUMMARYSHOWN,Boolean.toString(false),VistaOV);
+		
+		GrammarVO.getViews().add(Valor);
+		GrammarVO.getViews().add(Valor2);
+		GrammarVO.getViews().add(Valor3);
+		
+
+		String VistaOVMeta=new String(NameConstantsMIGS.META);
+		
+		CompleteOperationalValueType ValorMeta=new CompleteOperationalValueType(NameConstantsMIGS.TYPE,NameConstantsMIGS.VIRTUAL_OBJECT,VistaOVMeta);
+		
+		GrammarVO.getViews().add(ValorMeta);
+		
+		
+		String VistaOVOda=new String(NameConstantsMIGS.ODA);
+		
+		CompleteOperationalValueType ValorOda=new CompleteOperationalValueType(NameConstantsMIGS.PUBLIC,Boolean.toString(true),VistaOVOda);
+
+		CompleteOperationalValueType ValorOda2=new CompleteOperationalValueType(NameConstantsMIGS.PRIVATE,Boolean.toString(false),VistaOVOda);
+
+		GrammarVO.getViews().add(ValorOda);
+		GrammarVO.getViews().add(ValorOda2);
+		}
+		
+		
+		CC.getMetamodelGrammar().add(GrammarVO);
+		
+		CompleteTextElementType idovattr=new CompleteTextElementType(NameConstantsMIGS.IDOV,GrammarVO);
+		
+		{
+		String VistaOV=new String(NameConstantsMIGS.PRESNTACION);
+		
+		CompleteOperationalValueType VisibleAtt = new CompleteOperationalValueType(NameConstantsMIGS.VISIBLESHOWN,Boolean.toString(true),VistaOV);
+		CompleteOperationalValueType Valor2=new CompleteOperationalValueType(NameConstantsMIGS.BROWSERSHOWN,Boolean.toString(false),VistaOV);
+		CompleteOperationalValueType Valor3=new CompleteOperationalValueType(NameConstantsMIGS.SUMMARYSHOWN,Boolean.toString(false),VistaOV);
+		
+		idovattr.getShows().add(VisibleAtt);
+		idovattr.getShows().add(Valor2);
+		idovattr.getShows().add(Valor3);
+		
+		String VistaOVMeta=new String(NameConstantsMIGS.META);
+		
+		CompleteOperationalValueType ValorMeta=new CompleteOperationalValueType(NameConstantsMIGS.TYPE,NameConstantsMIGS.IDOV,VistaOVMeta);
+		
+		idovattr.getShows().add(ValorMeta);
+		
+		String VistaMetaType=new String(NameConstantsMIGS.METATYPE);
+		CompleteOperationalValueType MetaType=new CompleteOperationalValueType(NameConstantsMIGS.METATYPETYPE,NameConstantsMIGS.TEXT,VistaMetaType);
+		idovattr.getShows().add(MetaType);
+		}
+		
+		
+CompleteTextElementType ovType=new CompleteTextElementType(NameConstantsMIGS.OVType,GrammarVO);
+		
+		{
+		String VistaOV=new String(NameConstantsMIGS.PRESNTACION);
+		
+		CompleteOperationalValueType VisibleAtt = new CompleteOperationalValueType(NameConstantsMIGS.VISIBLESHOWN,Boolean.toString(true),VistaOV);
+		CompleteOperationalValueType Valor2=new CompleteOperationalValueType(NameConstantsMIGS.BROWSERSHOWN,Boolean.toString(false),VistaOV);
+		CompleteOperationalValueType Valor3=new CompleteOperationalValueType(NameConstantsMIGS.SUMMARYSHOWN,Boolean.toString(false),VistaOV);
+		
+		ovType.getShows().add(VisibleAtt);
+		ovType.getShows().add(Valor2);
+		ovType.getShows().add(Valor3);
+		
+		String VistaOVMeta=new String(NameConstantsMIGS.META);
+		
+		CompleteOperationalValueType ValorMeta=new CompleteOperationalValueType(NameConstantsMIGS.TYPE,NameConstantsMIGS.OVType,VistaOVMeta);
+		
+		ovType.getShows().add(ValorMeta);
+		
+		String VistaMetaType=new String(NameConstantsMIGS.METATYPE);
+		CompleteOperationalValueType MetaType=new CompleteOperationalValueType(NameConstantsMIGS.METATYPETYPE,NameConstantsMIGS.TEXT,VistaMetaType);
+		ovType.getShows().add(MetaType);
+		}
+		
+	}
+
 
 
 	@Override
---
+++ ClavyMIGSImportPlugin/src/fdi/ucm/server/migs/NameConstantsMIGS.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package fdi.ucm.server.migs;
+
+/**
+ * Clase que define las constantes de nombre para chasqui
+ * @author Joaquin Gayoso-Cabada
+ *
+ */
+public class NameConstantsMIGS {
+
+
+	//STATIC NAMES FOR METAS
+	public static final String METADATOSNAME = "Metadatos";
+	public static final String DATOSNAME = "Datos";
+	public static final String IDOVNAME = "IDOV";
+	public static final String VIRTUAL_OBJECTNAME="Virtual Object";
+	public static final String DESCRIPTIONNAME = "Description";
+	public static final String RESOURCENAME = "Resource";
+	public static final String FILENAME = "File";
+	public static final String IDOV_OWNERNAME = "Idov Owner";
+	public static final String TYPE_FILE_RECNAME="TypeRec";
+	public static final String TYPE_FILENAME = "Type";
+	public static final String IDNAME = "ID";
+	public static final String DISPLAY_NAMENAME = "Display Name";
+	public static final String VOCNUMBER = "Vocabulary N#";
+	public static final String OVType = "OVType";
+	
+	//	PRESENTACION
+	public static final String PRESNTACION = "Presentacion";
+	public static final String VISIBLESHOWN = "Visible";
+	public static final String BROWSERSHOWN = "Browseable";
+	public static final String SUMMARYSHOWN="Summary";
+	
+	//META
+	public static final String META = "Meta";
+	public static final String TYPE = "Type";
+	public static final String FILERESOURCE = "File Resource";
+	public static final String METADATOS = "Metadatos";
+	public static final String DATOS = "Datos";
+	public static final String IDOV = "IDOV";
+	public static final String VIRTUAL_OBJECT="Virtual Object";
+	public static final String DESCRIPTION = "Description";
+	public static final String RESOURCE = "Resource";
+	public static final String FILE = "File";
+	public static final String IDOV_OWNER = "Idov Owner";
+	
+	//ODA
+	public static final String ODA = "Oda";
+	public static final String PUBLIC = "Public";
+	public static final String PRIVATE = "Private";
+	public static final String EXTENSIBLE = "Extensible";
+	
+	//TYPE
+	public static final String METATYPE = "Meta Type";
+	public static final String METATYPETYPE = "Type";
+	public static final String NUMERIC = "Numeric";
+	public static final String TEXT = "Text";
+	public static final String DATE = "Date";
+	public static final String CONTROLED = "Controled";
+	
+	public static final String VOCABULARY = "Vocabulary";
+	public static final String TERM = "Term";
+
+
+}
